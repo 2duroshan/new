6335d97466eb3575ec7f3508e9addc773b9c3c6f
2duroshan <79957768+2duroshan@users.noreply.github.com>
2021-03-03 10:24:14 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"603f1389877fae00228a096d",
+				"603f1389877fae00228a096f"
+			]
+		},
+		"603f1389877fae00228a096d": {
+			"id": "603f1389877fae00228a096d",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"603f1389877fae00228a096f": {
+			"id": "603f1389877fae00228a096f",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
