c393ebec4dc274d6d9595939095a1612e63a22a4
2duroshan <79957768+2duroshan@users.noreply.github.com>
2021-03-03 10:41:00 +0530

    Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Link, Button } from "@quarkly/widgets";
+import { Theme, Link, Icon, Text, Box, Input, Button, Image } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
 import { GlobalQuarklyPageStyles } from "global-page-styles";
-import { GoogleMap } from "@quarkly/components";
+import { GoogleMap, Override, StackItem, Stack, Formspree, Section, SocialMedia } from "@quarkly/components";
+import { MdLocationOn, MdEmail, MdPhone, MdFace } from "react-icons/md";
+import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
 export default (() => {
 	return <Theme theme={theme}>
 		<GlobalQuarklyPageStyles pageUrl={"index"} />
@@ -15,11 +17,188 @@ export default (() => {
 			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
 		</Helmet>
 		<GoogleMap />
+		<Section
+			color="--light"
+			padding="100px 0"
+			sm-padding="40px 0"
+			position="relative"
+			background="linear-gradient(0deg,rgba(25, 30, 34, 0.8) 0%,rgba(25, 30, 34, 0.8) 100%),--color-darkL2 url(https://images.unsplash.com/photo-1547619292-240402b5ae5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80)"
+		>
+			<StackItem width="50%" md-width="100%">
+				<Override slot="StackItemContent" flex-direction="column" />
+				<Box
+					padding="0 0 0 64px"
+					sm-padding="64px 0 0 0"
+					margin="32px 0 0 0"
+					max-width="360px"
+					position="relative"
+				>
+					<Icon
+						position="absolute"
+						size="48px"
+						top="0"
+						left="0"
+						category="md"
+						icon={MdLocationOn}
+					/>
+					<Text as="h4" margin="6px 0" font="--base">
+						Visit us
+					</Text>
+					<Text as="p" margin="6px 0" font="--headline3">
+						4998 Hanover Street, New York, 10011
+					</Text>
+				</Box>
+				<Box
+					padding="0 0 0 64px"
+					sm-padding="64px 0 0 0"
+					margin="64px 0 0 0"
+					max-width="360px"
+					position="relative"
+				>
+					<Icon
+						position="absolute"
+						size="48px"
+						top="0"
+						left="0"
+						category="md"
+						icon={MdEmail}
+					/>
+					<Text as="h4" margin="6px 0" font="--base">
+						Email us
+					</Text>
+					<Text as="p" margin="6px 0" font="--headline3">
+						<Link href="mailto:hello@company.com" text-decoration="none" hover-text-decoration="underline" color="--light">
+							hello@company.com
+						</Link>
+					</Text>
+				</Box>
+				<Box padding="0 0 0 64px" margin="64px 0 0 0" max-width="360px" position="relative">
+					<Icon
+						position="absolute"
+						size="48px"
+						top="0"
+						left="0"
+						category="md"
+						icon={MdPhone}
+					/>
+					<Text as="h4" margin="6px 0" font="--base">
+						Call us
+					</Text>
+					<Text as="p" margin="6px 0" font="--headline3">
+						+1 (234) 567-89-00
+						<br />
+						+1 (234) 567-89-00
+					</Text>
+				</Box>
+				<Box
+					padding="0 0 0 64px"
+					sm-padding="0"
+					margin="48px 0"
+					max-width="360px"
+					position="relative"
+					display="flex"
+				>
+					<Icon
+						category="fa"
+						icon={FaFacebookF}
+						width="48px"
+						height="48px"
+						size="24px"
+						margin-right="16px"
+						background="--color-primary"
+						border-radius="50%"
+					/>
+					<Icon
+						category="fa"
+						icon={FaTwitter}
+						width="48px"
+						height="48px"
+						size="24px"
+						margin-right="16px"
+						background="--color-primary"
+						border-radius="50%"
+					/>
+					<Icon
+						category="fa"
+						icon={FaLinkedinIn}
+						width="48px"
+						height="48px"
+						size="24px"
+						margin-right="16px"
+						background="--color-primary"
+						border-radius="50%"
+					/>
+				</Box>
+			</StackItem>
+			<Stack>
+				<StackItem width="50%" md-width="100%">
+					<Box
+						max-width="360px"
+						padding="56px 48px"
+						margin="0 0 0 auto"
+						md-max-width="100%"
+						background="--color-primary"
+					>
+						<Text as="h3" font="--headline3" margin="0 0 20px 0">
+							Leave us message
+						</Text>
+						<Formspree>
+							<Stack gap="16px">
+								<StackItem width="100%">
+									<Override slot="StackItemContent" flex-direction="column" />
+									<Text font="--base" margin="0 0 4px 0">
+										Name
+									</Text>
+									<Input width="100%" name="name" />
+								</StackItem>
+								<StackItem width="100%">
+									<Override slot="StackItemContent" flex-direction="column" />
+									<Text font="--base" margin="0 0 4px 0">
+										Email
+									</Text>
+									<Input width="100%" type="email" name="email" />
+								</StackItem>
+								<StackItem width="100%">
+									<Override slot="StackItemContent" flex-direction="column" />
+									<Text font="--base" margin="0 0 4px 0">
+										Message
+									</Text>
+									<Input as="textarea" rows="4" width="100%" name="message" />
+								</StackItem>
+								<StackItem width="100%">
+									<Button background="--color-dark">
+										Send
+									</Button>
+								</StackItem>
+							</Stack>
+						</Formspree>
+					</Box>
+				</StackItem>
+			</Stack>
+			<Image width="64px" height="64px" />
+		</Section>
+		<Section background="--color-dark" padding="60px 0" sm-padding="40px 0">
+			<SocialMedia
+				facebook="https://www.facebook.com/quarklyapp/"
+				twitter="https://twitter.com/quarklyapp"
+				youtube="https://www.youtube.com/channel/UCK5bXs2L0bbSMQ82BQ3hIkw"
+				vkontakte="https://vk.com/quarklyapp"
+				telegram="https://t.me/joinchat/DqSYDhS0R9nMRvOtFbIxrQ"
+			>
+				<Override
+					slot="link"
+					border-radius="50%"
+					color="--grey"
+					hover-color="--light"
+					background="transparent"
+					hover-background="transparent"
+					margin="0 8px"
+				/>
+			</SocialMedia>
+		</Section>
+		<Icon category="md" icon={MdFace} />
 		<Link>
-			<Button>
-				GoogleMap
-			</Button>
-			https://www.google.com/maps
+			GoogleMap
 		</Link>
 		<Link
 			font={"--capture"}
